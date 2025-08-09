160f7d1b5031a10b24f7e7917dac2bad64651d2d
David and Daniil Liberman <da@liberman.net>
2025-08-09 12:42:11 +0200

    Bump versions

--- python/setup.py
+++ python/setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="gonka-openai",
-    version="0.2.1",
+    version="0.2.2",
     author="David Liberman",
     author_email="david@liberman.net",
     description="OpenAI client with Gonka network integration",
