81e7a9e0b013b2c496c8f17bdb8d86cecc09eb2b
Gleb Morgachev <morgachev.g@gmail.com>
2025-08-10 16:47:43 -0700

    Move files to enable pip install

--- python/setup.py
+++ python/setup.py
@@ -3,7 +3,7 @@
 Setup script for gonka-openai
 """
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -17,8 +17,9 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/product-science/gonka-openai",
-    packages=["gonka_openai"],
-    package_dir={"gonka_openai": "src"},
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
